1a65f05082ca4494d3c422ce90e21591be427e1b
goodeverything <easycomeeasygo55@163.com>
2017-07-25 13:16:03 +0800

    修改table组件

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,7 @@
+/**
+* Created by sunqi on 2017/7/24.
+*/
 import axios from 'axios';
-
 axios.defaults.baseURL = 'http://www.easy-mock.com/mock/596ee5e1a1d30433d837634c/sell';
 
 let base = '';
@@ -10,12 +12,11 @@ export const getUserList = params => { return axios.get(`${base}/user/list`, { p
 
 export const getUserListPage = params => { return axios.get(`${base}/user/listpage`, { params: params }); };
 
-export const removeUser = params => { return axios.get(`${base}/user/remove`, { params: params }); };
+export const removeUser = params => { return axios.delete(`${base}/user/remove`, { params: params }); };
 
 export const batchRemoveUser = params => { return axios.get(`${base}/user/batchremove`, { params: params }); };
 
-export const editUser = params => { return axios.get(`${base}/user/edit`, { params: params }); };
+export const editUser = params => { return axios.put(`${base}/user/edit`, { params: params }); };
 
-export const addUser = params => { return axios.get(`${base}/user/add`, { params: params }); };/**
- * Created by sunqi on 2017/7/24.
- */
+export const addUser = params => { return axios.post(`${base}/user/add`, { params: params }); };
+
--- src/views/nav1/Table.vue
+++ src/views/nav1/Table.vue
@@ -23,20 +23,48 @@
         </template>
       </el-table-column>
     </el-table>
+
     <el-row>
       <!--工具条-->
       <el-col :span="24" class="toolbar">
-        <el-button type="danger" @click="batchRemove" :disabled="this.selection.length===0">批量删除</el-button>
+        <el-button type="danger" @click="batchRemove" :disabled="selectedRows.length===0">批量删除</el-button>
         <el-pagination layout="prev, pager, next" @current-change="handleCurrentChange" :page-size="20" :total="total"
                        style="float:right;">
         </el-pagination>
       </el-col>
     </el-row>
+
+    <el-dialog title="编辑" :visible.sync="editFormVisible" close-on-click-modal="false">
+      <el-form :model="editForm" label-width="80px" :rules="editFormRules" ref="editForm">
+        <el-form-item label="姓名" prop="name">
+          <el-input v-model="editForm.name" auto-complete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="性别">
+          <el-radio-group v-model="editForm.sex">
+            <el-radio class="radio" :label="1">男</el-radio>
+            <el-radio class="radio" :label="0">女</el-radio>
+          </el-radio-group>
+        </el-form-item>
+        <el-form-item label="年龄">
+          <el-input-number v-model="editForm.age" :min="0" :max="200"></el-input-number>
+        </el-form-item>
+        <el-form-item label="生日">
+          <el-date-picker type="date" placeholder="选择日期" v-model="editForm.birth"></el-date-picker>
+        </el-form-item>
+        <el-form-item label="地址">
+          <el-input type="textarea" v-model="editForm.addr"></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click.native="editFormVisible = false">取消</el-button>
+        <el-button type="primary" @click.native="editSubmit" :loading="editLoading">提交</el-button>
+      </div>
+    </el-dialog>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
-  import {getUserListPage, batchRemoveUser} from '../../api/api';
+  import {getUserListPage, editUser, removeUser, batchRemoveUser} from '../../api/api';
   export default {
     data() {
       return {
@@ -46,7 +74,24 @@
         users: [],
         total: 0,
         page: 1,
-        selection: []
+        selectedRows: [],
+
+        editFormVisible: false,
+        editLoading: false,
+        editFormRules: {
+          name: [
+            {required: true, message: '请输入姓名', trigger: 'blur'}
+          ]
+        },
+
+        editForm: {
+          id: 0,
+          name: '',
+          sex: -1,
+          age: 0,
+          birth: '',
+          addr: ''
+        }
       };
     },
     methods: {
@@ -65,25 +110,66 @@
         this.getUsers();
       },
       handleSelectionChange(selection) {
-        this.selection = selection;
+        this.selectedRows = selection;
       },
       formatSex(row, column) {
         return row.sex === 1 ? '男' : row.sex === 0 ? '女' : '未知';
       },
+
+      handleEdit(index, row) {
+        this.editFormVisible = true;
+        this.editForm = Object.assign({}, row);
+      },
+      // 编辑
+      editSubmit() {
+        this.$refs.editForm.validate((valid) => {
+          if (valid) {
+            this.$confirm('确认提交吗？', '提示', {}).then(() => {
+              this.editLoading = true;
+              let para = Object.assign({}, this.editForm);
+              para.birth = (!para.birth || para.birth === '') ? '' : new Date(para.birth);
+              editUser(para).then((res) => {
+                this.editLoading = false;
+                this.$message({
+                  message: '提交成功',
+                  type: 'success'
+                });
+                this.$refs['editForm'].resetFields();
+                this.editFormVisible = false;
+                this.getUsers();
+              });
+            });
+          }
+        });
+      },
+      handleDel(index, row) {
+        this.$confirm('确认删除该记录吗?', '提示', {
+          type: 'warning'
+        }).then(() => {
+          let para = {id: row.id};
+          removeUser(para).then((res) => {
+            this.$message({
+              message: '删除成功',
+              type: 'success'
+            });
+          });
+          this.users.splice(index, 1);
+        });
+      },
       batchRemove() {
-        var ids = this.selection.map(item => item.id).toString();
+        var ids = this.selectedRows.map(item => item.id).toString();
+        console.log(ids);
         this.$confirm('确认删除选中记录吗？', '提示', {
           type: 'warning'
         }).then(() => {
           let para = {ids: ids};
           batchRemoveUser(para).then((res) => {
-            this.listLoading = false;
             this.$message({
               message: '删除成功',
               type: 'success'
             });
-            this.getUsers();
           });
+          this.getUsers();
         });
       }
     },
